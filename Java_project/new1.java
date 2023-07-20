8559b191bf3679d030b5a011dd51acf07a6911db
dineshpoun <132435714+dineshpoun@users.noreply.github.com>
2023-07-20 15:12:32 +0530

    a changed to 22

--- Java_project/new1.java
+++ Java_project/new1.java
@@ -2,7 +2,7 @@ package Java_project;
 
 public class new1 {
     public static void main(String[] args) {
-        int a=10;
+        int a=22;
         System.out.println("hello");
         
     }
